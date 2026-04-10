c7328995b057672bb76675867ca02e739a1d002f
Muhammad Rifky <muh.rifky.harto@gmail.com>
2026-04-10 21:09:10 +0700

    fix mysql host connection

--- mqtt-service/mqtt-listeners.js
+++ mqtt-service/mqtt-listeners.js
@@ -16,7 +16,7 @@ const client = mqtt.connect("mqtt://localhost:1883");
 const db = mysql.createConnection({
   host: "127.0.0.1",
   user: "root",
-  password: "",
+  password: "123456",
   database: "iot_system"
 });
 
